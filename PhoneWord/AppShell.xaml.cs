4e59fa5bc39baf7e9899cc6276813984c8312025
User <User@MY-LENOVO>
2023-12-24 00:32:45 +0000

    Add project files.

---
+++ PhoneWord/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneWord
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PhoneWord/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhoneWord
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
